1cef19dad73f5a9cab83e27bd554e619231dd0a4
Suraj Pradhan <surajprofile1000@gmail.com>
2023-08-07 15:10:51 +0530

    change the node path

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("node:path");
+const path = require('path');
 const { connectDatabase, Subscriber, Blog } = require("./config/mongo");
 const multer = require("multer");
 
